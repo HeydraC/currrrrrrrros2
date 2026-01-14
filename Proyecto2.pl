72058ca13f35575451c90dca558c96c627b36187
Carlos Heydra <heydracarlos7@gmail.com>
2026-01-13 20:17:08 -0400

    Fix vehicle type from 'h' to 'v' in Proyecto2.pl

--- Proyecto2.pl
+++ Proyecto2.pl
@@ -35,7 +35,7 @@ moveVehicle(ListaOriginal, ID, Steps, ListaNueva) :-
     
     NewRow is Row + Steps,
     
-    NuevoVehiculo = v(ID, h, NewRow, Col, Len),
+    NuevoVehiculo = v(ID, v, NewRow, Col, Len),
     ListaNueva = [NuevoVehiculo | RestoDeVehiculos].
 
 moveVehicle(ListaOriginal, ID, Steps, ListaNueva) :-
